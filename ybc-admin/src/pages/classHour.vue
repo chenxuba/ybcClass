24a8310f9fdc4a8c1d3162945a392ba85fba0c21
chenxuba <1115009958@qq.com>
2019-12-06 18:41:41 +0800

    “备注”

--- ybc-admin/src/pages/classHour.vue
+++ ybc-admin/src/pages/classHour.vue
@@ -3,7 +3,7 @@
     <div class="tabs">
       <el-tabs type="card" v-model="activeName">
         <el-tab-pane name="first">
-          <span slot="label">
+          <span slot="label" @click="tabClick">
             <i class="el-icon-view"></i> 课时
           </span>
           <childClass
@@ -52,7 +52,7 @@ export default {
     shuaxinListsss(item) {
       this.getClassHourList();
       this.activeName = item;
-      this.tabsNmae = '新建课时'
+      this.tabsNmae = "新建课时";
     },
     shuxinLists() {
       console.log(111);
@@ -65,6 +65,56 @@ export default {
       this.$refs.childNewClass.activeName = tabs;
       this.$refs.childNewClass.tabsDisabled = true;
       this.$refs.childNewClass.getEditMsg(item);
+    },
+    tabClick() {
+      if (this.tabsNmae == "编辑课时") {
+        this.$confirm(
+          "是否要暂时保存内容，确定后编辑的内容会暂时保存!",
+          "提示",
+          {
+            confirmButtonText: "确定",
+            cancelButtonText: "取消",
+            type: "warning"
+          }
+        )
+          .then(() => {
+            this.$notify({
+              title: "成功",
+              message: "编辑的内容已经暂时保存～",
+              type: "success",
+              position: "top-left"
+            });
+          })
+          .catch(() => {
+            this.tabsNmae = "新建课时";
+            this.$refs.childNewClass.ruleForm = {
+              date: "", //直播开始时间
+              title: "", //标题
+              imgUrl: "", //封面图
+              content: "", //直播简介
+              leixing1: "", //一级标签
+              leixing2: "", //二级标签
+              radio_fufei: 1, //收费方式
+              radio_isShikan: 0, //是否需要试看,默认不需要
+              yinSiSet: "0", //隐私设置
+              shangJiaSet: 1, //上架设置，是否立即发布
+              price: "", //收费金额
+              password: "", // 密码
+              shikanTime: "", //试看时间
+              videoUrl: "",
+              dingShiTime: new Date(), //定时直播
+              res_id: "",
+              leixing: [],
+              associate_sell: "", //是否关联售卖，默认不关联
+              associate_type: 99, //关联资源类型，是关联课程还是打卡学堂 99-课程 100-打卡学堂 默认关联课程
+              course_id: "", //关联课程ID
+              clockin_id: "" //关联打卡学堂ID
+            };
+            this.$refs.childNewClass.handleClick();
+          });
+      }
+      // 点击课程tabs让相关课tabs消失
+      this.showXiangguanke = false;
     }
   },
   mounted() {
