0af6e89fbcca107e4a20aab1c8aa75e32c6f2f8f
chenxuba <1115009958@qq.com>
2019-12-19 11:55:02 +0800

    “备注”

--- src/pages/xsCourseDetail.vue
+++ src/pages/xsCourseDetail.vue
@@ -1,7 +1,7 @@
 <template>
-  <div v-height>
+  <div v-height class="box">
     线下课程详情
-    {{id}}
+    <div v-html="XxCourseDetail.introduce"></div>
   </div>
 </template>
 
@@ -10,13 +10,15 @@ import { reqXxCourseDetail } from "../api";
 export default {
   data() {
     return {
-      id: this.$route.params.id
+      id: this.$route.params.id,
+        XxCourseDetail:""
     };
   },
   methods: {
     async getXxCourseDetail() {
       const result = await reqXxCourseDetail(this.id, "");
       console.log(result);
+      this.XxCourseDetail = result.data
     }
   },
   mounted() {
@@ -26,4 +28,11 @@ export default {
 </script>
 
 <style scoped>
+.box{
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+.box >>> img{
+    width: 100%;
+}
 </style>
--- src/pages/yaoyueCart.vue
+++ src/pages/yaoyueCart.vue
@@ -1,12 +1,34 @@
 <template>
   <div v-height class="box">
-      邀约卡
+    <div class="warp">
+      <img width="100%" :src="imgurl" />
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      imgurl: `https://api.ybc365.com/api/5dbfb44893afe?cookie=${sessionStorage.getItem(
+        "cookie"
+      )}&type=${this.$route.query.type}&res_id=${this.$route.query.id}&id=2`
+    };
+  },
+  methods: {},
+  mounted() {
+    console.log(this.imgurl);
+  }
+};
 </script>
 
 <style scoped>
+.box{
+  padding-top: 80px;
+  box-sizing: border-box;
+}
+.warp{
+  width: 80%;
+  margin: 0 auto;
+}
 </style>
